87358ba71b808f31f482936594e1a60493af62e7
Oliver-PC\Oliver <ohauser@hm.edu>
2018-03-21 21:03:38 +0100

    created layout

--- lib/main.dart
+++ lib/main.dart
@@ -32,18 +32,68 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           new Expanded(
             child: new Container(
-              color: Colors.lightGreen,
+              color: Colors.black87,
+              padding: const EdgeInsets.all(10.0),
               child: new Align(
                 alignment: Alignment.centerRight,
                 child: new Text(
                   "80 * 20",
-                  style: new TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 50.0),
+                  style: new TextStyle(fontSize: 50.0, color: Colors.white70),
                 ),
               ),
             ),
           ),
+          new Expanded(
+            flex: 3,
+            child: new Container(
+              color: Colors.black87,
+              child: new Column(
+                children: <Widget>[
+                  makeButtons("C_%/"),
+                  makeButtons("789x"),
+                  makeButtons("456-"),
+                  makeButtons("123+"),
+                  makeButtons("0,=")
+                ],
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget makeButtons(String row) {
+    List<String> token = row.split("");
+    return new Expanded(
+      flex: 1,
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: token.map((e) => new CalcButton(keyvalue: e)).toList(),
+      ),
+    );
+  }
+}
+
+class CalcButton extends StatelessWidget {
+  CalcButton({this.keyvalue});
+
+  final String keyvalue;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Container(
+        margin: const EdgeInsets.all(0.5),
+        child: new FlatButton(
+            color: Colors.white,
+            child: new Center(
+              child: new Text(
+                keyvalue,
+                style: new TextStyle(fontSize: 30.0),
+              ),
+            ),
+            onPressed: () {}),
       ),
     );
   }
